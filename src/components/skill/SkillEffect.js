080bafff1f330ec416c6db26ac8848eaeba10bd0
Shin <cjfals3154@naver.com>
2022-06-08 07:57:20 +0900

    update skill

--- src/components/skill/SkillEffect.js
+++ src/components/skill/SkillEffect.js
@@ -1,24 +1,37 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import { gsap, Power3 } from "gsap";
 
 function SkillEffect({ children, text }) {
   const [iconClick, setIconClick] = useState("");
+  const logoItem = useRef(null);
+  const logoText = useRef(null);
 
-  const onChangeIcon = useCallback((e) => {
-    setIconClick();
-  }, []);
+  const onLeave = ({ currentTarget }) => {
+    gsap.to(currentTarget, { scale: 1.0 });
+  };
+
+  const onEnter = ({ currentTarget }) => {
+    gsap.to(currentTarget, { scale: 1.33 });
+  };
+
+  // const onChangeIcon = useCallback((e) => {
+  //   setIconClick();
+  // }, []);
 
   return (
     <div>
       <div
         className="skill-icon transition-[transform] rotate-[0deg] ease-in-out duration-700"
-        onClick={onChangeIcon}
+        // onClick={onChangeIcon}
+        onMouseEnter={onEnter}
+        onMouseLeave={onLeave}
+        ref={logoItem}
       >
         {children}
       </div>
       <div
-        style={{ paddingLeft: 60, paddingRight: 60 }}
-        style={{ fontWeight: 700 }}
+        style={{ fontWeight: 700, marginTop: 15 }}
         className="absolute bottom-[-2.5rem] sm:bottom-[-2rem] left-[50%] translate-x-[-50%] w-full font-bold text-sm text-center "
       >
         {text}
